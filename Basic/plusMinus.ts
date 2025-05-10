c377a3bac8802bc5f90e47168b25f7a91502f5b7
nahidn228 <nahidn228@gmail.com>
2025-05-10 23:50:22 +0600

    update readLine

---
+++ Basic/plusMinus.ts
@@ -0,0 +1,30 @@
+/**
+ * Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6  places after the decimal.
+
+Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to 10^-4  are acceptable.
+ */
+
+
+const plusMinus = (arr: number[]): any => {
+  let n = arr.length;
+  let positive = 0;
+  let negative = 0;
+  let zero = 0;
+  for (const num of arr) {
+    if (num > 0) {
+      positive++;
+    } else if (num < 0) {
+      negative++;
+    } else {
+      zero++;
+    }
+  }
+  const positiveRatio = (positive / n).toFixed(6);
+  const negativeRatio = (negative / n).toFixed(6);
+  const zeroRatio = (zero / n).toFixed(6);
+   
+  console.log({ positiveRatio, negativeRatio, zeroRatio });
+};
+
+const arr = [1, 1, 0, -1, -1];
+plusMinus(arr);
---
+++ Basic/readLine.ts
@@ -0,0 +1,13 @@
+/**
+ * Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+ */
+
+const miniMaxSum = (arr: number[]) => {
+  arr.sort((a, b) => a - b);
+  const minSum = arr.slice(0, 4).reduce((sum, num) => sum + num, 0);
+  const maxSum = arr.slice(1, 5).reduce((sum, num) => sum + num, 0);
+  console.log(`${minSum} ${maxSum}`);
+};
+
+const arr1 = [1, 3, 5, 7, 9];
+miniMaxSum(arr1);
